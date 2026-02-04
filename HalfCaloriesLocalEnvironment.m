0a869ddd45de68e22ecd218f3bcacf0c36eea49d
jsgerber <58957808+jamesgerber@users.noreply.github.com>
2026-02-04 17:59:22 +0100

    add an example local environment file

---
+++ HalfCaloriesLocalEnvironment.m
@@ -0,0 +1,8 @@
+codedir=['~/source/HalfTheCaloriesProducedAvailableAsFood/'];
+
+base=['/Users/jsgerber/sandbox/jsg220_FoodFeedAllocation_PublicationRevision/'];
+workingdirectory=base;
+SAGEAdminRastersDir=[base 'inputdata/Reference/AdminRastersSage'];;
+databasedir=[base 'inputdata/Reference/ChadCropmaps/Global/database/'];
+OceanMDBDataDir=[base 'inputdata/Reference/OutputOceanmdb/'];
+GeoDatabaseDirectory=[base 'GeodatabaseFiles/'];
